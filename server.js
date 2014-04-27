602e6744a9fb5ed493752f72ab950fe929c01e05
Vlad Tsepelev <reg.yeti@gmail.com>
2014-04-27 13:17:53 +0300

    simplify bot to use post back to slack outgoing hook

--- server.js
+++ server.js
@@ -10,34 +10,19 @@ app.get('/', function(req, res){
 app.post('/boobs', function(req, res) {
 	console.dir(req);
 	
-	// var backDomain = req.body.team_domain; // chat to be use to back post to Slack
-	// var backToken = (req.query && req.query.back_token) || "Z89M3k3cnifutUcwSlmTz1Rp"; // token to be use for post back to Slack
-
 	function randomInt(high) {
 		return Math.floor(Math.random() * high);
 	}
 
 	var boobsUrl = "http://api.oboobs.ru/boobs/" + randomInt(5000) + "/1/rank";
-	request.get(boobsUrl, function(err) {
+	request.get(boobsUrl, function(err, response) {
 
 		var payload = {
-			// channel: "#general",
-			// username: "boobsbot",
-			text: "http://media.oboobs.ru/" + JSON.parse(res.body)[0].preview
-			// icon_emoji: ":ghost:"
+			text: "http://media.oboobs.ru/" + JSON.parse(response.body)[0].preview
 		};
-
-		// var url = "http://requestb.in/13i0nly1";
-		// var url = "https://"+backHost+"/services/hooks/incoming-webhook?token="+backToken;
-		// var url = "https://"+backDomain+".slack.com/services/hooks/incoming-webhook?token=Z89M3k3cnifutUcwSlmTz1Rp";
 
 		res.send(JSON.stringify(payload));
 		res.end();
-		// request.post({
-		// 	url: url,
-		// 	body: JSON.stringify(payload)
-		// });
-
 	});
 	
 });

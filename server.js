9c3b3e20b736f52e101e71a7a82ae5b804fdd070
Vlad Tsepelev <reg.yeti@gmail.com>
2014-04-27 12:36:06 +0300

    post to any Slack subdomain by using tokens and hostnames from request

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ app.get('/', function(req, res){
 	res.send("Smart Enterprise Boobs Manager!");
 });
 
-app.post('/boobs', function(req, res) {
+app.get('/boobs', function(req, res) {
+	// console.dir(req);
+	
+	var backHost = req.headers.host;
+	var backToken = req.query.back_token; // token to be use for post back to Slack
+
 
 	function randomInt(high) {
 		return Math.floor(Math.random() * high);
@@ -24,7 +29,7 @@ app.post('/boobs', function(req, res) {
 		};
 
 		// var url = "http://requestb.in/no93g5no";
-		var url = "https://freechat.slack.com/services/hooks/incoming-webhook?token=Z89M3k3cnifutUcwSlmTz1Rp";
+		var url = "https://"+backHost+"/services/hooks/incoming-webhook?token="+backToken;
 
 		request.post({
 			url: url,
